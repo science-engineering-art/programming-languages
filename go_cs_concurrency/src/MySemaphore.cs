0fde724f5ff79e1e2b9dde5907db590189b95ce1
LRiera24 <laura.riera2412@gmail.com>
2022-12-10 21:09:18 -0500

    MyCountdownEvent and MySemaphore classes added

---
+++ go_cs_concurrency/src/MySemaphore.cs
@@ -0,0 +1,61 @@
+public class MySemaphore
+{
+    // Reference object for locks
+    private static object LockObj = new object();
+
+    // Number of threads that can enter the semaphore currently
+    public int Count { get; set; }
+    // Maximum number of threads that can enter the semaphore
+    public int Capacity { get; set; }
+    // Name of the semaphore (nullable)
+    public string? Name { get; set; }
+
+    public MySemaphore(int initial_entries, int maximum_entries)
+    {
+        if (maximum_entries < initial_entries)
+            throw new System.Exception("Number of threads in semaphore cannot be greater than its capacity.");
+
+        this.Count = initial_entries;
+        this.Capacity = maximum_entries;
+    }
+
+    public MySemaphore(int initial_entries, int maximum_entries, string name)
+    {
+        if (maximum_entries < initial_entries)
+            throw new System.Exception("Number of threads in semaphore cannot be greater than its capacity.");
+
+        this.Count = initial_entries;
+        this.Capacity = maximum_entries;
+        this.Name = name;
+    }
+
+    // A thread enters the semaphore
+    public void WaitOne()
+    {
+        lock (LockObj)
+        {
+            if (this.Count == 0)
+            {
+                Console.WriteLine("Semaphore is full, waiting...");
+                System.Threading.Monitor.Wait(LockObj);
+            }
+            this.Count--;
+        }
+    }
+    // An N quantity of threads release the semaphore
+    public int Release(int N = 1)
+    {
+        lock (LockObj)
+        {
+            this.Count += N;
+            if (this.Count > this.Capacity)
+            {
+                throw new Exception("Semaphore is empty, nothing to release.");
+            }
+            System.Threading.Monitor.PulseAll(LockObj);
+        }
+        return this.Count - N;
+    }
+}
+
+
